5d3ac5d662d0592686b3ef65bb5e0f87dfba8cca
Fabio Caprara <fats140701@gmail.com>
2022-05-01 22:38:03 -0400

    Validação do botão implementada

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
+let verificaModelo;
+let verificaGola;
+let verificaTecido;
+let url;
+let idInterval;
+let urlAPI = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
+let modeloEscolhido;
+let golaEscolhida;
+let tecidoEscolhido;
+
 let nome = prompt("Qual é o seu nome?");
 
 function selecionarModelo(modeloClicado){
@@ -7,7 +17,18 @@ function selecionarModelo(modeloClicado){
         modeloSelecionado.classList.remove("selecionado");
     }
 
-    modeloClicado.classList.add("selecionado");
+    modeloClicado.querySelector(".container-imagem-div").classList.add("selecionado");
+
+    let elementoSelecionado = document.querySelector(".secao-escolher-modelo .selecionado");
+
+    if(elementoSelecionado !== null){
+        verificaModelo = true;
+    } else {
+        verificaModelo = false;
+    }
+
+    modeloEscolhido = modeloClicado.querySelector("h3").innerHTML;
+    console.log(modeloEscolhido);
 }
 
 function selecionarGola(golaClicada){
@@ -17,7 +38,18 @@ function selecionarGola(golaClicada){
         golaSelecionada.classList.remove("selecionado");
     }
 
-    golaClicada.classList.add("selecionado");
+    golaClicada.querySelector(".container-imagem-div").classList.add("selecionado");
+
+    let elementoSelecionado = document.querySelector(".secao-escolher-gola .selecionado");
+
+    if(elementoSelecionado !== null){
+        verificaGola = true;
+    } else {
+        verificaGola = false;
+    }
+
+    golaEscolhida = golaClicada.querySelector("h3").innerHTML;
+    console.log(golaEscolhida);
 }
 
 function selecionarTecido(tecidoClicado){
@@ -27,5 +59,41 @@ function selecionarTecido(tecidoClicado){
         tecidoSelecionado.classList.remove("selecionado");
     }
 
-    tecidoClicado.classList.add("selecionado");
+    tecidoClicado.querySelector(".container-imagem-div").classList.add("selecionado");
+
+    let elementoSelecionado = document.querySelector(".secao-escolher-tecido .selecionado");
+
+    if(elementoSelecionado !== null){
+        verificaTecido = true;
+    } else {
+        verificaTecido = false;
+    }
+
+    tecidoEscolhido = tecidoClicado.querySelector("h3").innerHTML;
+    console.log(tecidoEscolhido);
+}
+
+function verificarPedido(){
+    let botaoConfirmar = document.querySelector(".btn-confirmar-pedido");
+    console.log("verificando...")
+    if(verificaModelo && verificaGola && verificaTecido && validaURL()){
+        botaoConfirmar.classList.add("btn-selecionado");
+        botaoConfirmar.removeAttribute("disabled");
+    } else {
+        botaoConfirmar.classList.remove("btn-selecionado");
+        document.querySelector(".container-btn").innerHTML = `
+            <button class="btn-confirmar-pedido" onclick="confirmarEncomenda()" disabled>Confirmar pedido</button>
+        `
+    }
+}
+
+function validaURL() {
+    const regularExpression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+    url = document.querySelector(".input-imagem-referencia").value;
+    return regularExpression.test(url);
+}
+
+function confirmarEncomenda(){
+
+    const promise = axios.post(urlAPI)
 }
